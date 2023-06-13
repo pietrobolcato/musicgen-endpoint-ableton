602df5a504da34ac37e91c31f0d3c29f6b11d179
Pietro Bolcato <pietrobolcato@gmail.com>
2023-06-13 19:10:28 +0000

    feat: scoring script functions

--- aws/endpoint/src/code/inference.py
+++ aws/endpoint/src/code/inference.py
@@ -1,11 +1,14 @@
 """Inference code for the model"""
 
+import json
 import logging
+import os
 import time
-from typing import Any
 
 import torch
-from helpers import helpers
+from audiocraft.models import musicgen
+
+from aws.endpoint.src.helpers import utils
 
 # define logging settings
 logging.basicConfig(
@@ -15,13 +18,27 @@ logging.basicConfig(
 )
 
 
-def model_fn(model_dir: str) -> Any:
-    """Loads the models"""
+def model_fn(model_dir: str) -> musicgen.MusicGen:
+    """
+    Loads the model
+
+    Parameters
+    ----------
+    model_dir : str
+        The root path of the model, i.e. the root path of the tar.gz file
+
+    Returns
+    -------
+    musicgen.MusicGen
+        The loaded musicgen model
+    """
     logging.info(f"CUDA available: {torch.cuda.is_available()}")
     logging.info("Loading the model...")
     start_time = time.time()
 
-    model = None  # fill this
+    # set paths for model loading
+    os.environ["MUSICGEN_ROOT"] = os.path.join(model_dir, "artifacts/")
+    model = musicgen.MusicGen.get_pretrained("medium", device="cuda")
 
     end_time = time.time()
     logging.info(f"Model loading completed in {(end_time - start_time):.2f} seconds")
@@ -29,17 +46,93 @@ def model_fn(model_dir: str) -> Any:
     return model
 
 
-def predict_fn(input_data: bytes, model: Any) -> dict:
-    """Perform inference based on the request"""
+def input_fn(request_body: str, content_type: str) -> dict:
+    """
+    Preprocess the input data. Checks that its in json format, and returns a python
+    dictionary
+
+    Parameters
+    ----------
+    request_body : str
+        The body of the request
+    content_type : str
+        The content type of the request
+
+    Returns
+    -------
+    dict
+        The json string parsed to python dictionary
+    """
+    assert content_type == "application/json", "Only `application/json` is supported"
+
+    # TODO: add schema validation
+
+    if isinstance(request_body, str):
+        request_body = json.loads(request_body)
+
+    return request_body
+
+
+def predict_fn(input_data: dict, model: musicgen.MusicGen) -> tuple:
+    """
+    Perform inference based on the request
+
+    Parameters
+    ----------
+    input_data : dict
+        The request input data, returned by the `input_fn` function
+    model : musicgen.MusicGen
+        The musicgen loaded model
+
+    Returns
+    -------
+    tuple
+        A tuple containing the output audio in base64 encoded string, and the processing
+        time
+    """
     start_time = time.time()
 
     logging.info(f"Received request with data: {input_data}")
 
-    result = None  # fill this
+    utils.set_generation_parameters(input_data, model)
+    prediction = model.generate([input_data.get("prompt")], progress=False)
+    prediction_base64 = utils.audio_to_base64(
+        audio=prediction[0],
+        sample_rate=model.sample_rate,
+    )
 
     end_time = time.time()
     processing_time_ms = int((end_time - start_time) * 1000)
 
+    return prediction_base64, processing_time_ms
+
+
+def output_fn(prediction: tuple, content_type: str) -> dict:
+    """
+    Returns the prediction of `predict_fn` in JSON format
+
+    Parameters
+    ----------
+    prediction : tuple
+        The prediction returned from the `predict_fn` function
+    content_type : str
+        The request output content type
+
+    Returns
+    -------
+    dict:
+        The output json in dictionary format
+    """
+    assert content_type == "application/json", "Only `application/json` is supported"
+
+    logging.info("Output function called")
+    logging.info(f"Content type: {content_type}")
+
+    prediction_base64, processing_time_ms = prediction
+
     return {
-        "result": {"prediction": result, "processing_time_ms": processing_time_ms},
+        "result": {
+            "prediction": prediction_base64,
+            "processing_time_ms": processing_time_ms,
+        },
     }
--- aws/endpoint/tests/test_inference.py
+++ aws/endpoint/tests/test_inference.py
@@ -6,9 +6,29 @@ from pathlib import Path
 ENDPOINT_SRC_ROOT_DIR = Path(__file__).parents[1].resolve() / "src"
 sys.path.append(str(ENDPOINT_SRC_ROOT_DIR))
 
-import pytest
+import json
 
 import aws.endpoint.src.code.inference as inference
-import aws.endpoint.tests.utils as aws_tests_utils
 
-# to fill
+# import pytest
+
+# import aws.endpoint.tests.utils as aws_tests_utils
+
+request_body = json.dumps(
+    {
+        "prompt": "berghain acid techno",
+        "duration": 1,
+        "temperature": 1.0,
+        "top_p": 0.0,
+        "top_k": 250,
+        "cfg_coefficient": 3.0,
+    },
+)
+
+model = inference.model_fn(model_dir=ENDPOINT_SRC_ROOT_DIR)
+input_data = inference.input_fn(
+    request_body=request_body,
+    content_type="application/json",
+)
+prediction = inference.predict_fn(input_data=input_data, model=model)
+output = inference.output_fn(prediction=prediction, content_type="application/json")
