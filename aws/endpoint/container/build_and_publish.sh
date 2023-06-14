9df590754c393e1dad9b58f1c0a56fbce0afd590
Pietro Bolcato <pietrobolcato@gmail.com>
2023-06-14 21:29:27 +0000

    fix: build and push to private ecr

--- aws/endpoint/container/build_and_publish.sh
+++ aws/endpoint/container/build_and_publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # build and push the docker image
-PUBLIC_ECR_LINK="public.ecr.aws/s0f8z6e9"
+ECR_LINK="138140302683.dkr.ecr.us-east-1.amazonaws.com"
 IMAGE_NAME="musicgen-pytorch:1.0"
 
-docker build -t "$IMAGE_NAME" -t "$PUBLIC_ECR_LINK"/"$IMAGE_NAME" .
-docker push "$PUBLIC_ECR_LINK"/"$IMAGE_NAME"
+docker build -t "$IMAGE_NAME" -t "$ECR_LINK"/"$IMAGE_NAME" .
+docker push "$ECR_LINK"/"$IMAGE_NAME"
